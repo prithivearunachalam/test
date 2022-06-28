d9e41420950a5a882daa425f388c138bf5df5d11
prithivearunachalam <prithivearun@gmail.com>
2022-06-28 13:17:03 -0400

    Adding 2 js files

---
+++ test1.js
@@ -0,0 +1,17 @@
+<html>
+
+<h1> Test Document1 </h1>
+<p> This section covers the test document reference for git actions  </p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<body>
+
+<script>
+document.getElementbyId("Demo").innerHTML = "TestDemo";
+
+</script>
+</body>
+
+
+</html>
---
+++ test2.js
@@ -0,0 +1,17 @@
+<html>
+
+<h1> Test Document1 </h1>
+<p> This section covers the test document reference for git actions  </p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<body>
+
+<script>
+document.write("Test statement");
+
+</script>
+</body>
+
+
+</html>
